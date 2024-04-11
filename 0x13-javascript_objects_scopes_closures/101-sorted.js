6e01203968df18202ae79b44994dc038e5d92b13
Justicesibisi <djmangsa@gmail.com>
2024-04-11 00:13:42 +0000

    last try

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
+const { dict } = require('./101-data');
+
+const convertedArr = Object.entries(dict);
+
+const newObj = {};
+
+convertedArr.forEach(element => {
+  newObj[element[1]] ? newObj[element[1]].push(element[0]) : newObj[element[1]] = [element[0]];
 });
-console.log(mappedList);
+
+console.log(newObj);
